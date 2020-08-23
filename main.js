cf58bf884e3955358e72c42b1d76cbe16f3144b7
Annette <69789074+anniemt6@users.noreply.github.com>
2020-08-23 14:33:06 +0300

    pockemon-game

--- main.js
+++ main.js
@@ -1,3 +1,71 @@
-alert("Hello, Zar!");
+const character = {
+	name: 'Pikachu',
+	defaultHP: 100,
+	damageHP: 100,
+	healthCharacter: document.getElementById('health-character'),
+	progressBar: document.getElementById('progressbar-character'),
+}
+
+const enemy = {
+	name: 'Charmander',
+	defaultHP: 100,
+	damageHP: 100,
+	healthCharacter: document.getElementById('health-enemy'),
+	progressBar: document.getElementById('progressbar-enemy'),
+}
+
+//функция инициализации проекта
+function init(){
+	console.log('start game');
+	changeHealthe(character);
+	changeHealthe(enemy);
+}	
+init();
+
+
+//функция изменения параметров персонажей
+function changeHealthe(person){
+	changeHPLife(person);
+	changeProgressbarHP(person);
+}
+
+//меняем текстовое поле жизни персонажа
+function changeHPLife(person){
+	person.healthCharacter.innerText = person.damageHP + '/' + person.defaultHP;
+	
+}
+
+//меняем прогресс бар персонажа
+function changeProgressbarHP(person){
+	person.progressBar.style.width = person.damageHP + '%';
+}
+
+//функция нанесения удара: кому и сколько
+function attack (person, count, button){
+	
+	if(person.damageHP < count){ person.damageHP = 0;  button.disabled = true; alert(person.name + ' проиграл бой!!');}
+	else { person.damageHP -= count; }
+	changeHealthe(person);
+}
+
+//генерация рандомного удара
+function randomCountAttack(num){
+	return Math.ceil(Math.random()*num);
+}
+
+
+
+//из дом берем айди кнопок, для дальнейшей работы
+const klick = document.getElementById('btn-kick');
+const klickAttackEnemy = document.getElementById('btn-kick-electro');
+
+
+
+//"оживляем" кнопки
+klick.addEventListener('click', function () {attack(character, randomCountAttack(10), klick); attack(enemy, randomCountAttack(10), klick);} );
+klickAttackEnemy.addEventListener('click', function() { attack(enemy, 10, klickAttackEnemy);} );
+
+
+
 
 
